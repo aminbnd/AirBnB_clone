f67ddd71b2ea9ac3a029bcc40efec8c1d881c015
aminbnd <2212@holbertonschool.com>
2021-02-15 20:47:02 +0100

    pep8 fixed, remained a long ligne to reduce

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ import models
 date = "%Y-%m-%dT%H:%M:%S.%f"
 """Defines the BaseModel class """
 
+
 class BaseModel:
     """the BaseModel of the HBnB projectss """
     id = str(uuid4())
@@ -13,12 +14,13 @@ class BaseModel:
 
     def __str__(self):
         return ("[{}] {} {}".format(self.__class__.__name__, self.id, self.__dict__))
+
     def save(self):
         self.updated_at = datetime.now()
+
     def to_dict(self):
         dict_1 = self.__dict__.copy()
         dict_1["created_at"] = self.created_at.isoformat()
         dict_1["updated_at"] = self.updated_at.isoformat()
         dict_1["__class__"] = self.__class__.__name__
         return dict_1
-    
