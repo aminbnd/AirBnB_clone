11d562ed458fb7d93d8f6553d0c83b15a73408e5
mehdizit <zitounimehdi.it@gmail.com>
2021-02-15 21:56:43 +0100

    update for task 4 **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,28 @@
 from uuid import uuid4
 from datetime import datetime
 import models
-date = "%Y-%m-%dT%H:%M:%S.%f"
+
 """Defines the BaseModel class """
 
 class BaseModel:
     """the BaseModel of the HBnB projectss """
-    id = str(uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initialize attributes and created, updated the date"""
+        date="%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for k, v in kwargs.items():
+                if "created_at" == k:
+                    self.created_at = datetime.strptime(kwargs["created_at"], date)
+                elif "updated_at" == k:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"], date)
+                elif "__class__" == k:
+                    pass
+                else:
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return ("[{}] {} {}".format(self.__class__.__name__, self.id, self.__dict__))
@@ -21,4 +35,5 @@ class BaseModel:
         dict_1["updated_at"] = self.updated_at.isoformat()
         dict_1["__class__"] = self.__class__.__name__
         return dict_1
+
     
