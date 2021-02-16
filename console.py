ef45ef86cc486262e340ca528e398811983b15c4
mehdizit <zitounimehdi.it@gmail.com>
2021-02-16 21:53:53 +0100

    updated for task 7 create new instance and print the new id object with interpreter

--- console.py
+++ console.py
@@ -2,13 +2,15 @@
 """Define Command interpreter"""
 
 import cmd
+from models.base_model import   BaseModel
+
 class HBNBCommand(cmd.Cmd):
     """Class for command interpreter
     Attributes:
         prompt (str): Command prompt.
     """
-
     prompt = "(hbnb) "
+    classes = {"BaseModel"}
     def do_EOF(self, line):
         """To exit with Ctrl-D"""
         print("")
@@ -16,5 +18,17 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, line):
         """quit command to exit the program"""
         return True
+    def do_create(self, line):
+        """Create instance of class"""
+        if len(line) == 0:
+            print("** class name missing **")
+        elif line not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        else:
+            instance = eval(line)()
+            instance.save()
+            print(instance.id)
+
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
