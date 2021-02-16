bbdca40504a3f3bbf22a6bfc6363617fea742856
mehdizit <zitounimehdi.it@gmail.com>
2021-02-17 00:23:41 +0100

    add user, state, city, place, review classes inherit from BaseModel

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define user class"""
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    '''class user that inherit from BaseModel'''
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
