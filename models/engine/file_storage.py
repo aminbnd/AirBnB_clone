81129bf441845d3f7bca0d2920a124a3d9e1bf1c
aminbnd <2212@holbertonschool.com>
2021-02-17 16:47:55 +0100

    Fix pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,14 +18,17 @@ class FileStorage:
     class_dict = {"BaseModel": BaseModel, "User": User, "Place": Place,
                   "Amenity": Amenity, "City": City, "Review": Review,
                   "State": State}
+
     def all(self):
         """returns the dictionary __objects """
         return self.__objects
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id """
         if obj:
             key = '{}.{}'.format(obj.__class__.__name__, obj.id)
             self.__objects[key] = obj
+
     def save(self):
         """serializes __objects to the JSON file """
         my_dict = {}
@@ -33,6 +36,7 @@ class FileStorage:
             my_dict[key] = obj.to_dict()
         with open(self.__file_path, 'w') as f:
             json.dump(my_dict, f)
+
     def reload(self):
         """ deserializes the JSON file to __objects """
         try:
